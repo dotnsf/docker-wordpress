e2dd167a40479a2b6ecc521b7613e75a23cce848
K.Kimura <dotnsf@gmail.com>
2021-03-08 18:35:01 +0900

    servername can be specified in wordpress shell scripts

--- docker_rm_wordpress.sh
+++ docker_rm_wordpress.sh
@@ -2,12 +2,16 @@
 
 
 if [ $# -eq 0 ]; then
-	echo "Usage: $0 [num]";
+	echo "Usage: $0 [num] [servername]";
 	exit 1
 else
 	NUM="${1:-1}"
+	SERVERNAME="wp$NUM.mydockernet.local"
+    if [ $# -gt 1 ]; then
+		SERVERNAME="${2}"
+    fi
 
-	docker rm wp"$NUM".mydockernet.local
+	docker rm "$SERVERNAME"
 	docker exec mysql mysql -u root -proot -e 'drop database wp'$NUM'db'
 	rm ./nginx/wp$NUM.conf
 fi
--- docker_run_wordpress.sh
+++ docker_run_wordpress.sh
@@ -2,12 +2,15 @@
 
 
 if [ $# -eq 0 ]; then
-	echo "Usage: $0 [num]";
+	echo "Usage: $0 [num] [servername]";
 	exit 1
 else
 	NUM="${1:-1}"
 	PORT=`expr $NUM \+ 8080`
 	SERVERNAME="wp$NUM.mydockernet.local"
+    if [ $# -gt 1 ]; then
+		SERVERNAME="${2}"
+    fi
 
 	docker run --name "$SERVERNAME" --network my-wp-network --link mysql:mysql -e WORDPRESS_DB_HOST=mysql:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=root -e WORDPRESS_DB_NAME=wp"$NUM"db -d -p "$PORT":80 wordpress
 
