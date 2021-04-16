8e981339ccf22d7d72d880494c48a9c0274c1699
K.Kimura <dotnsf@gmail.com>
2021-04-16 22:24:41 +0900

    linuxserver/mariadb

--- docker_run_mysql.sh
+++ docker_run_mysql.sh
@@ -2,7 +2,7 @@
 
 docker network create my-wp-network
 
-docker run --name mysql --network my-wp-network -e MYSQL_ROOT_PASSWORD=root -d mysql
+docker run --name mysql --network my-wp-network -e MYSQL_ROOT_PASSWORD=root -d linuxserver/mariadb
 
 docker run --name nginx --network my-wp-network -v `pwd`/nginx:/etc/nginx/conf.d -d -p 80:80 nginx
 
