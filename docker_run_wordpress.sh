50d0bb49df8107bfb0cfc1da0ff8ca0de2beaf2a
K.Kimura <dotnsf@gmail.com>
2021-04-17 17:14:38 +0900

    deprecated --link parameter removed.

--- docker_run_wordpress.sh
+++ docker_run_wordpress.sh
@@ -14,7 +14,7 @@ else
 
 	mkdir ./www/"$SERVERNAME"
 
-	docker run --name "$SERVERNAME" --network my-wp-network -v `pwd`/www/"$SERVERNAME":/var/www/html --link mysql:mysql -e WORDPRESS_DB_HOST=mysql:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=root -e WORDPRESS_DB_NAME=wp"$NUM"db -d -p "$PORT":80 wordpress
+	docker run --name "$SERVERNAME" --network my-wp-network -v `pwd`/www/"$SERVERNAME":/var/www/html -e WORDPRESS_DB_HOST=mysql:3306 -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=root -e WORDPRESS_DB_NAME=wp"$NUM"db -d -p "$PORT":80 wordpress
 
 	echo "server{ listen 80; server_name $SERVERNAME; location / { proxy_pass http://$SERVERNAME; proxy_redirect default; } }" > ./nginx/wp"$NUM".conf
 
